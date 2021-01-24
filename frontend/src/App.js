8121d4caa1c1f58f5bc81f8f8748489373e19158
Indiana Lora <indiana@ensaludybelleza.com>
2021-01-24 17:39:54 -0400

    add the frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function Header() {
+  return (
+    <div className="Header">
+      <header className="App-header" style={style}>
+        <h1>Bolsa de empleos</h1>
+      </header>
+    </div>
+  );
+}
+const style = {
+  
+};
+
+export default Header;
