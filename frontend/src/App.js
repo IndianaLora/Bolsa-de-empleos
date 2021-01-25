8c3d04d61d894ee25324e406200e9ff04302d6e3
Indiana Lora <indiana@ensaludybelleza.com>
2021-01-25 12:19:59 -0400

    delete files not needed at frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,25 @@ function Header(props) {
     <div className="Header">
       <header className="App-header">
         {/* <h3>Clock {props.date.toLocaleTimeString()}</h3> */}
-        <h1>Empleos RD</h1>
+        <h1>
+          Empleos
+          <span id="r"> R</span>
+          <span id="d">D</span>
+        </h1>
       </header>
+      {function Forms() {
+        return (
+          <form>
+            <label>
+              Name:
+              <input type="text" name="name" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        );
+      }}
     </div>
   );
 }
-// function createClock(props) {
-//   ReactDOM.render(<Header date={new Date()} />);
-// }
 
 export default Header;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,18 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Header from "./App";
-// import createClock from "./App";
+import Forms from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <createClock /> */}
     <Header />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("header")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
