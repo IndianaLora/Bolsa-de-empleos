fa220d12f48f42866e21d3631ad31217889778d7
Indiana Lora <indiana@ensaludybelleza.com>
2021-01-29 12:35:20 -0400

    styles frontend form

--- frontend/src/container.js
+++ frontend/src/container.js
@@ -9,73 +9,88 @@ export default function Container() {
   return (
     <div id="container">
       <form id="form" onSubmit={handleSubmit(onSubmit)}>
-        <label>
-          Compañía:
-          <input
-            type="text"
-            name="Compañía"
-            ref={register({ required: true, minLength: 2 })}
-          />
-        </label>
-
-        <br></br>
-        <label>
-          Tipo :
-          <select name="Tipo" ref={register}>
-            <option value="">Agente</option>
-            <option value="">Supervisor</option>
-            <option value=""> Gerente</option>
-          </select>
-          {errors.Tipo && <p>Debe llenar este campo</p>}
-        </label>
-
-        <br></br>
-        <label>
-          Logo (opcional):
-          <input type="text" name=" Logo" ref={register} />
-        </label>
-
-        <br></br>
-        <label>
-          URL (opcional):
-          <input type="text" name="URL " ref={register} />
-        </label>
-
-        <br></br>
-        <label>
-          Posición (puesto):
-          <input type="text" name="Posición" ref={register} />
-        </label>
-
-        <br></br>
-        <label>
-          Ubicación:
-          <select name="ubicacion" ref={register}>
-            <option value=""> Santo Domingo</option>
-            <option value=""> Zona oriental</option>
-          </select>
-        </label>
-        <br></br>
-        <label>
-          Categoría :
-          <input type="text" name="Categoría" ref={register} />
-        </label>
-        <br></br>
-        <label>
-          Descripción :
-          <input type="text" name=" Descripción" ref={register} />
-        </label>
-        <br></br>
-        <label>
-          Cómo aplicar:
-          <input type="text" name="Cómo aplicar" ref={register} />
-        </label>
-        <br></br>
-        <label>
-          Email (Compañía):
-          <input type="text" name=" Email" ref={register} />
-        </label>
-        <br></br>
+        <input
+          type="text"
+          placeholder="  Compañía:"
+          name="Compañía"
+          ref={register({ required: true, minLength: 2 })}
+          id="form-input"
+        />
+        <select
+          placeholder=" Tipo :"
+          name="Tipo"
+          ref={register}
+          id="form-input"
+        >
+          <option value="">Agente</option>
+          <option value="">Supervisor</option>
+          <option value=""> Gerente</option>
+        </select>
+        {errors.Tipo && <p>Debe llenar este campo</p>}
+        <input
+          type="text"
+          placeholder=" Logo (opcional):"
+          name=" Logo"
+          ref={register}
+          id="form-input"
+        />
+        <input
+          type="text"
+          placeholder="URL (opcional):"
+          name="URL "
+          ref={register}
+          id="form-input"
+        />
+        <input
+          type="text"
+          placeholder="(puesto):"
+          name="Posición"
+          ref={register}
+          id="form-input"
+        />
+        <select
+          name="ubicacion"
+          placeholder="Ubicación:"
+          ref={register}
+          id="form-input"
+        >
+          <option value=""> Santo Domingo</option>
+          <option value=""> Zona oriental</option>
+        </select>
+        <input
+          type="text"
+          name="Categoría"
+          ref={register}
+          placeholder="categoria"
+          id="form-input"
+        />
+        <input
+          type="text"
+          placeholder=" Descripción :"
+          name=" Descripción"
+          ref={register}
+          id="form-input"
+        />
+        <input
+          type="text"
+          placeholder="Cómo aplicar:"
+          name="Cómo aplicar"
+          ref={register}
+          id="form-input"
+        />
+        <input
+          type="text"
+          placeholder=" Email (Compañía):"
+          name=" Email"
+          ref={register}
+          id="form-input"
+        />
+        <div id="message">
+          <textarea
+            placeholder="Escribe una descripcion para el puesto de trabajo"
+            id="form-input"
+          ></textarea>
+        </div>
         <input type="submit" value="Enviar" id="button" />
       </form>
     </div>
