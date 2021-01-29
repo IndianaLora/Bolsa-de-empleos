d3c6675244edbed1e7fd38a62fc515b29c8e6f17
Indiana Lora <indiana@ensaludybelleza.com>
2021-01-29 14:21:35 -0400

    style form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,25 @@ import "./App.css";
 import ReactDOM from "react-dom";
 function Header(props) {
   return (
-    <div className="Header">
-      <header className="App-header">
-        <h1>
-          <h2>Empleos <span id="R">R</span><span id="D">D</span></h2>
-          <li id="nav">
-            <a href="">Inicio</a>
-            <a href="">Sobre nosotros</a>
-            <a href="">Contacto</a>
-          </li>
-        </h1>
+    <div className="header">
+      
+   
+          <h2>
+            Empleos <span id="R">R</span>
+            <span id="D">D</span>
+          </h2>
+          <div>
+      <header className="header-nav">
+        
+        <nav id="nav">
+          <a href="">Inicio...</a>
+          <a href="">Sobre nosotros...</a>
+          <a href="">Contacto...</a>
+        </nav>
       </header>
     </div>
+    </div>
+   
   );
 }
 
