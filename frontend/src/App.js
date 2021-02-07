1d4f8f910d5a06726838d3fd4c73cad543b0f4b1
Indiana Lora <indiana@ensaludybelleza.com>
2021-02-07 17:09:08 -0400

    added 'vista previa' to the post jobs form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import Busqueda from "./components/busqueda/Busqueda";
-import Container from "./Container";
+import Container from "./container";
 import Navbar from "./components/navbar/navbar";
 import Cards from "./components/cards/cards";
 import Login from "./components/Form/login";
 import Registro from "./components/Form/registro";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 
 export default function App() {
   return (
--- frontend/src/container.js
+++ frontend/src/container.js
@@ -10,10 +10,7 @@ export default function Container() {
   const home = () => {
     history.push("/busqueda");
   };
-  // const editar = () => {
-  //   popUp.style = "visibility: hidden";
-  // };
-  function onSubmit(data) {
+  const onSubmit = (data) => {
     const pop = (
       <div>
         <img src={data.logo} />
@@ -26,115 +23,119 @@ export default function Container() {
         <h5>Como aplicar: {data.posicion}</h5>
         <h5>Email: {data.email}</h5>
         <h5>descripcion: {data.descripcion}</h5>
-        <button className="button">Editar</button>
-        <button className="button" onClick={home}>
-          Continuar
-        </button>
+        <div>
+          <button className="editar">Editar</button>
+          <button className="finalizar" onClick={home}>
+            Finalizar
+          </button>
+        </div>
       </div>
     );
     var popUp = document.getElementById("popUp");
     ReactDom.render(pop, popUp);
     popUp.style = "visibility: visible";
-  }
+  };
 
-  export default function Container() {
-    const { register, handleSubmit, errors } = useForm();
-    const onSubmit = (data) => console.log(data);
-
-    return (
-      <div id="container" className="container">
-        <div className="popUp" id="popUp">
-          {" "}
-        </div>
-        <form id="form" onSubmit={handleSubmit(onSubmit)}>
-          <input
-            type="text"
-            placeholder="  Compañía:"
-            name="compañía"
-            ref={register({ required: true, minLength: 2 })}
-            id="form-input"
-          />
-          <select
-            placeholder=" Tipo :"
-            name="tipo"
-            ref={register}
-            id="form-input"
-          >
-            <option value="">Agente</option>
-            <option value="">Supervisor</option>
-            <option value=""> Gerente</option>
-          </select>
-          {errors.Tipo && <p>Debe llenar este campo</p>}
-          <input
-            type="image"
-            placeholder=" Logo (opcional):"
-            name=" logo"
-            ref={register}
-            id="form-input"
-          />
-          <input
-            type="text"
-            placeholder="URL (opcional):"
-            name="url"
-            ref={register}
-            id="form-input"
-          />
-          <input
-            type="text"
-            placeholder="(puesto):"
-            name="posicion"
-            ref={register}
-            id="form-input"
-          />
-          <select
-            name="ubicacion"
-            placeholder="Ubicación:"
-            ref={register}
-            id="form-input"
-          >
-            <option value=""> Santo Domingo</option>
-            <option value=""> Zona oriental</option>
-          </select>
-          <input
-            type="text"
-            name="categoria"
-            ref={register}
-            placeholder="categoria"
-            id="form-input"
-          />
-          <input
-            type="text"
-            placeholder=" Descripción :"
-            name="descripcion"
-            ref={register}
-            id="form-input"
-          />
-          <input
-            type="text"
-            placeholder="Cómo aplicar:"
-            name="comoAplicar"
-            ref={register}
-            id="form-input"
-          />
-          <input
-            type="text"
-            placeholder=" Email (Compañía):"
-            name="email"
-            ref={register}
-            id="form-input"
-          />
-          <div id="descripcion">
-            <textarea
-              placeholder="Escribe una descripcion para el puesto de trabajo"
-              id="form-input"
-              name="descripcion"
-            >
-              {" "}
-            </textarea>
-          </div>
-          <input type="submit" value="Enviar" className="button" />
-        </form>
+  return (
+    <div id="container" className="container">
+      <div className="popUp" id="popUp">
+        {" "}
       </div>
-    );
-  }
+      <form id="form" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Nombre de la empresa:"
+          name="empresa"
+          ref={register({ required: true, minLength: 2 })}
+          id="form-input"
+        />
+        <select
+          placeholder=" Tipo :"
+          name="tipo"
+          ref={register}
+          id="form-input"
+        >
+          <option value="">Agente</option>
+          <option value="">Supervisor</option>
+          <option value=""> Gerente</option>
+        </select>
+        <div>
+          <label for="tipo">Tiempo completo</label>
+          <input type="radio" name="tipo" value="Tiempo completo" />
+          <label for="tipo">Medio Tiempo</label>
+          <input type="radio" name="tipo" value="Tiempo completo" />
+          <label for="tipo">Free-lancer</label>
+          <input type="radio" name="tipo" value="Tiempo completo" />
+        </div>
+
+        {errors.Tipo && <p>Debe llenar este campo</p>}
+        <input
+          type="image"
+          placeholder=" Logo (opcional):"
+          name=" logo"
+          ref={register}
+          id="form-input"
+        />
+        <input
+          type="text"
+          placeholder="URL (opcional):"
+          name="url"
+          ref={register}
+          id="form-input"
+        />
+        <input
+          type="text"
+          placeholder="(puesto):"
+          name="posicion"
+          ref={register}
+          id="form-input"
+        />
+        <select
+          name="ubicacion"
+          placeholder="Ubicación:"
+          ref={register}
+          id="form-input"
+        >
+          <option value=""> Santo Domingo</option>
+          <option value=""> Zona oriental</option>
+        </select>
+        <input
+          type="text"
+          name="categoria"
+          ref={register}
+          placeholder="categoria"
+          id="form-input"
+        />
+        <input
+          type="text"
+          placeholder=" Descripción :"
+          name="descripcion"
+          ref={register}
+          id="form-input"
+        />
+        <select name="aplicar" ref={register} id="form-input">
+          <option value="">Como aplicar :</option>
+          <option value="">Correo electronico</option>
+          <option value="">Numero telefonico</option>
+        </select>
+        <input
+          type="text"
+          placeholder=" Email (Compañía):"
+          name="email"
+          ref={register}
+          id="form-input"
+        />
+        <div id="descripcion">
+          <textarea
+            placeholder="Escribe una descripcion para el puesto de trabajo"
+            id="form-input"
+            name="descripcion"
+          >
+            {" "}
+          </textarea>
+        </div>
+        <input type="submit" value="Enviar" className="button" />
+      </form>
+    </div>
+  );
 }
