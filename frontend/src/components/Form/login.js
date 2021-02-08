a114ae6f28f6853aa099cc99efef3a1fd88243b4
Carlos Diaz <carlos12fds151@gmail.com>
2021-02-08 13:36:23 -0400

    Consumiendo login para que lo tomen de ejemplo.

--- frontend/src/components/Form/login.js
+++ frontend/src/components/Form/login.js
@@ -1,9 +1,26 @@
+import axios from "axios";
 import { useForm } from "react-hook-form";
 import "./login.css";
 export default function Login() {
   const { register, handleSubmit, errors } = useForm();
   console.log(errors);
-  const onSubmit = (data) => console.log(data.usuario, data.password);
+  const onSubmit = (data) => {
+    let credentials = {
+      
+    }
+    axios
+    .post("http://localhost:3000/login/signIn", {
+      username:data.usuario,
+      password:data.password
+    })
+    .then(function(res) {
+      console.log(JSON.parse(res.request.response));
+    })
+    .catch(function (err) {
+     console.log(JSON.parse(err.request.response));
+    });
+    console.log(credentials);
+  };
   return (
     <div className="container">
       <form class="login-box" onSubmit={handleSubmit(onSubmit)}>
